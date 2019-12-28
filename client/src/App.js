f57d2a9e79ba2c40b78ecb9b95624c362ae17f61
sidorovi4 <vladsidorovi4@gmail.com>
2019-12-28 22:29:58 +0300

    jwt token ready"

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router} from 'react-router-dom'
-import 'materialize-css'
 import { useRoutes } from './routes'
+import { useAuth } from './hooks/auth.hook'
+import { AuthContext } from './context/auth.context'
+import { Navbar } from './components/Navbar'
+import { Loader } from './components/Loader'
+import 'materialize-css'
 
 function App() {
-  const routes = useRoutes(false)
+  const { token, login, logout, userId, ready } = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
+
+  if (!ready) {
+    return <Loader />
+  }
   return (
-    <Router>
-      <div className="container">
-        { routes }
-      </div>
-    </Router>
+    <AuthContext.Provider value = {{ token, login, logout, userId, isAuthenticated }}>
+      <Router>
+        { isAuthenticated && <Navbar />}
+        <div className="container">
+          { routes }
+        </div>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,40 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect, useContext} from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
+import { AuthContext } from '../context/auth.context'
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext)
+  const message = useMessage()
+  const {loading, request, error, clearError} = useHttp()
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
+
+  useEffect(() => {
+    message(error)
+    clearError()
+  }, [error, message, clearError])
+
+  useEffect(() => {
+    window.M.updateTextFields()
+  }, [])
+
+  const changeHandler = event => {
+    setForm({...form, [event.target.name]: event.target.value})
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form})
+      message(data.message)
+    } catch(e) {
+
+    }
+  }
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form})
+      auth.login(data.token, data.userId)
+    } catch(e) {
+
+    }
+  }
+
   return (
     <div className="row">
       <div className="col s6 offset-s3">
-        <h1>Сократи ссылку</h1>
+        <h3>Сократи ссылку</h3>
         <div className="card blue darken-1">
         <div className="card-content white-text">
           <span className="card-title">Авторизация</span>
           <div>
-          
-          <div className="input-field">
-            <input
-              placeholder="Введите email"
-              id="email"
-              type="text"
-              name="email"
-              className="yellow-input"
-            />
-            <label htmlFor="email">Email</label>
-          </div>
-          <div className="input-field">
+            <div className="input-field">
+              <input
+                placeholder="Введите email"
+                id="email"
+                type="text"
+                name="email"
+                className="yellow-input"
+                value={form.email}
+                onChange={changeHandler}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field">
             <input
               placeholder="Введите пароль"
               id="password"
               type="password"
               name="password"
               className="yellow-input"
+              value={form.password}
+              onChange={changeHandler}
+              disabled={loading}
             />
             <label htmlFor="password">Пароль</label>
           </div>
           </div>
         </div>
         <div className="card-action">
-          <button class="btn yellow darken-4" style={{marginRight: 10}}> Войти</button>
-          <button class="btn grey lighten-1"> Регистрация </button>
+          <button  
+           className="btn yellow darken-4"
+           style={{marginRight: 10}}
+           onClick={loginHandler}
+           disabled={loading}
+          > Войти</button>
+          <button disabled={loading} onClick={registerHandler} className="btn grey lighten-1"> Регистрация </button>
         </div>
       </div>
       </div>
